8578b701e4be4504cfafd2e0ab5ac448df1366e0
tqdung11a3 <truongquocdung2004@gmail.com>
2025-05-08 10:57:27 +0700

    Tích hợp css js header

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,396 @@
-console.log("OK");
+// Menu Mobile
+const buttonMenuMobile = document.querySelector(".header .inner-menu-mobile");
+if (buttonMenuMobile) {
+    const menu = document.querySelector(".header .inner-menu");
+
+    // Click vào button mở menu
+    buttonMenuMobile.addEventListener("click", () => {
+        menu.classList.add("active");
+    });
+
+    // Click vào overlay đóng menu
+    const overlay = document.querySelector(".header .inner-overlay");
+    if (overlay) {
+        overlay.addEventListener("click", () => {
+            menu.classList.remove("active");
+        });
+    }
+
+    // Click vào icon down mở sub menu
+    const listButtonSubMenu = menu.querySelectorAll("ul > li > i");
+    listButtonSubMenu.forEach((button) => {
+        button.addEventListener("click", () => {
+            button.parentNode.classList.toggle("active");
+        });
+    });
+}
+// End Menu Mobile
+
+// Box Address Section 1
+const boxAddressSection1 = document.querySelector(
+    ".section-1 .inner-form .inner-address"
+);
+if (boxAddressSection1) {
+    // Ẩn/hiện box suggest
+    const input = boxAddressSection1.querySelector(".inner-input");
+
+    input.addEventListener("focus", () => {
+        boxAddressSection1.classList.add("active");
+    });
+
+    input.addEventListener("blur", () => {
+        boxAddressSection1.classList.remove("active");
+    });
+
+    // Sự kiện click vào từng item
+    const listItem = boxAddressSection1.querySelectorAll(
+        ".inner-suggest-list .inner-item"
+    );
+    listItem.forEach((item) => {
+        item.addEventListener("mousedown", () => {
+            const title = item
+                .querySelector(".inner-item-title")
+                .innerHTML.trim();
+            input.value = title;
+        });
+    });
+}
+// End Box Address Section 1
+
+// Box User Section 1
+const boxUserSection1 = document.querySelector(
+    ".section-1 .inner-form .inner-user"
+);
+if (boxUserSection1) {
+    // Hiện box quantity
+    const input = boxUserSection1.querySelector(".inner-input");
+
+    input.addEventListener("focus", () => {
+        boxUserSection1.classList.add("active");
+    });
+
+    // Ẩn box quantity
+    document.addEventListener("click", (event) => {
+        if (!boxUserSection1.contains(event.target)) {
+            boxUserSection1.classList.remove("active");
+        }
+    });
+
+    // Thêm số lượng vào ô input
+    const updateQuantityInput = () => {
+        const listBoxNumber = boxUserSection1.querySelectorAll(
+            ".inner-count .inner-number"
+        );
+        const listNumber = [];
+        listBoxNumber.forEach((boxNumber) => {
+            const number = parseInt(boxNumber.innerHTML);
+            listNumber.push(number);
+        });
+        const value = `NL: ${listNumber[0]}, TE: ${listNumber[1]}, EB: ${listNumber[2]}`;
+        input.value = value;
+    };
+
+    // Bắt sự kiện click nút up
+    const listButtonUp = boxUserSection1.querySelectorAll(
+        ".inner-count .inner-up"
+    );
+    listButtonUp.forEach((button) => {
+        button.addEventListener("click", () => {
+            const parent = button.parentNode;
+            const boxNumber = parent.querySelector(".inner-number");
+            const number = parseInt(boxNumber.innerHTML);
+            const numberUpdate = number + 1;
+            boxNumber.innerHTML = numberUpdate;
+            updateQuantityInput();
+        });
+    });
+
+    // Bắt sự kiện click nút down
+    const listButtonDown = boxUserSection1.querySelectorAll(
+        ".inner-count .inner-down"
+    );
+    listButtonDown.forEach((button) => {
+        button.addEventListener("click", () => {
+            const parent = button.parentNode;
+            const boxNumber = parent.querySelector(".inner-number");
+            const number = parseInt(boxNumber.innerHTML);
+            if (number > 0) {
+                const numberUpdate = number - 1;
+                boxNumber.innerHTML = numberUpdate;
+                updateQuantityInput();
+            }
+        });
+    });
+}
+// End Box User Section 1
+
+// Clock Expire
+const clockExpire = document.querySelector("[clock-expire]");
+if (clockExpire) {
+    const expireDateTimeString = clockExpire.getAttribute("clock-expire");
+
+    // Chuyển đổi chuỗi thời gian thành đối tượng Date
+    const expireDateTime = new Date(expireDateTimeString);
+
+    // Hàm cập nhật đồng hồ
+    const updateClock = () => {
+        const now = new Date();
+        const remainingTime = expireDateTime - now; // quy về đơn vị mili giây
+        if (remainingTime > 0) {
+            // Tính số ngày: mỗi ngày 24 * 60 * 60 * 1000 mili giây
+            const days = Math.floor(remainingTime / (24 * 60 * 60 * 1000));
+
+            // Tính số giờ: mỗi giờ 60 * 60 * 1000 mili giây
+            const hours = Math.floor((remainingTime / (60 * 60 * 1000)) % 24);
+
+            // Tính số phút: mỗi phút 60 * 1000 mili giây
+            const minutes = Math.floor((remainingTime / (60 * 1000)) % 60);
+
+            // Tính số phút: mỗi giây 1000 mili giây
+            const seconds = Math.floor((remainingTime / 1000) % 60);
+
+            const listBoxNumber = clockExpire.querySelectorAll(".inner-number");
+            listBoxNumber[0].innerHTML = `${days}`.padStart(2, "0");
+            listBoxNumber[1].innerHTML = `${hours}`.padStart(2, "0");
+            listBoxNumber[2].innerHTML = `${minutes}`.padStart(2, "0");
+            listBoxNumber[3].innerHTML = `${seconds}`.padStart(2, "0");
+        } else {
+            clearInterval(invervalClock);
+        }
+    };
+
+    // Gọi hàm cập nhật đồng hồ mỗi giây
+    const invervalClock = setInterval(updateClock, 1000);
+}
+// End Clock Expire
+
+// Box Filter
+const buttonFilterMobile = document.querySelector(
+    ".section-9 .inner-filter-mobile"
+);
+if (buttonFilterMobile) {
+    const boxLeft = document.querySelector(".section-9 .inner-left");
+
+    buttonFilterMobile.addEventListener("click", () => {
+        boxLeft.classList.add("active");
+    });
+
+    const overlay = document.querySelector(
+        ".section-9 .inner-left .inner-overlay"
+    );
+    overlay.addEventListener("click", () => {
+        boxLeft.classList.remove("active");
+    });
+}
+// End Box Filter
+
+// Box Tour Info
+const boxTourInfo = document.querySelector(".box-tour-info");
+if (boxTourInfo) {
+    const buttonReadMore = boxTourInfo.querySelector(".inner-read-more button");
+    buttonReadMore.addEventListener("click", () => {
+        if (boxTourInfo.classList.contains("active")) {
+            boxTourInfo.classList.remove("active");
+            buttonReadMore.innerHTML = "Xem tất cả";
+        } else {
+            boxTourInfo.classList.add("active");
+            buttonReadMore.innerHTML = "Ẩn bớt";
+        }
+    });
+
+    new Viewer(boxTourInfo);
+}
+// End Box Tour Info
+
+// Khởi tạo AOS
+AOS.init();
+// Hết Khởi tạo AOS
+
+// Swiper Section 2
+const swiperSection2 = document.querySelector(".swiper-section-2");
+if (swiperSection2) {
+    new Swiper(".swiper-section-2", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        breakpoints: {
+            992: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        },
+    });
+}
+// End Swiper Section 2
+
+// Swiper Section 3
+const swiperSection3 = document.querySelector(".swiper-section-3");
+if (swiperSection3) {
+    new Swiper(".swiper-section-3", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+        },
+    });
+}
+// End Swiper Section 3
+
+// Swiper Box Images
+const boxImages = document.querySelector(".box-images");
+if (boxImages) {
+    const swiperBoxImagesThumb = new Swiper(".swiper-box-images-thumb", {
+        spaceBetween: 5,
+        slidesPerView: 4,
+        breakpoints: {
+            576: {
+                spaceBetween: 10,
+            },
+        },
+    });
+
+    var swiperBoxImagesMain = new Swiper(".swiper-box-images-main", {
+        spaceBetween: 0,
+        thumbs: {
+            swiper: swiperBoxImagesThumb,
+        },
+    });
+}
+// End Swiper Box Images
+
+// Zoom Box Images Main
+const boxImagesMain = document.querySelector(".box-images .inner-images-main");
+if (boxImagesMain) {
+    new Viewer(boxImagesMain);
+}
+// End Zoom Box Images Main
+
+// Box Tour Schedule
+const boxTourSchedule = document.querySelector(".box-tour-schedule");
+if (boxTourSchedule) {
+    new Viewer(boxTourSchedule);
+}
+// End Box Tour Schedule
+
+// Email Form
+const emailForm = document.querySelector("#email-form");
+if (emailForm) {
+    const validator = new JustValidate("#email-form");
+
+    validator
+        .addField("#email-input", [
+            {
+                rule: "required",
+                errorMessage: "Vui lòng nhập email của bạn!",
+            },
+            {
+                rule: "email",
+                errorMessage: "Email không đúng định dạng!",
+            },
+        ])
+        .onSuccess((event) => {
+            const email = event.target.email.value;
+            console.log(email);
+        });
+}
+// End Email Form
+
+// Coupon Form
+const couponForm = document.querySelector("#coupon-form");
+if (couponForm) {
+    const validator = new JustValidate("#coupon-form");
+
+    validator
+        .addField("#coupon-input", [
+            {
+                rule: "required",
+                errorMessage: "Vui lòng nhập mã coupon!",
+            },
+        ])
+        .onSuccess((event) => {
+            const coupon = event.target.coupon.value;
+            console.log(coupon);
+        });
+}
+// End Coupon Form
+
+// Order Form
+const orderForm = document.querySelector("#order-form");
+if (orderForm) {
+    const validator = new JustValidate("#order-form");
+
+    validator
+        .addField("#full-name-input", [
+            {
+                rule: "required",
+                errorMessage: "Vui lòng nhập họ tên!",
+            },
+            {
+                rule: "minLength",
+                value: 5,
+                errorMessage: "Họ tên phải có ít nhất 5 ký tự!",
+            },
+            {
+                rule: "maxLength",
+                value: 50,
+                errorMessage: "Họ tên không được vượt quá 50 ký tự!",
+            },
+        ])
+        .addField("#phone-input", [
+            {
+                rule: "required",
+                errorMessage: "Vui lòng nhập số điện thoại!",
+            },
+            {
+                rule: "customRegexp",
+                value: /(84|0[3|5|7|8|9])+([0-9]{8})\b/g,
+                errorMessage: "Số điện thoại không đúng định dạng!",
+            },
+        ])
+        .onSuccess((event) => {
+            const fullName = event.target.fullName.value;
+            const phone = event.target.phone.value;
+            const note = event.target.note.value;
+            const method = event.target.method.value;
+
+            console.log(fullName);
+            console.log(phone);
+            console.log(note);
+            console.log(method);
+        });
+
+    // List Input Method
+    const listInputMethod = orderForm.querySelectorAll(`input[name="method"]`);
+    const elementInfoBank = orderForm.querySelector(".inner-info-bank");
+
+    listInputMethod.forEach((inputMethod) => {
+        inputMethod.addEventListener("change", () => {
+            if (inputMethod.value == "bank") {
+                elementInfoBank.classList.add("active");
+            } else {
+                elementInfoBank.classList.remove("active");
+            }
+        });
+    });
+}
+// End Order Form
