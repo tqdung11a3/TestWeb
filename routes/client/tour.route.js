f00c4423f705a5d8d5132c247197906ed6f52bba
tqdung11a3 <truongquocdung2004@gmail.com>
2025-05-09 18:50:14 +0700

    Finish view client

--- routes/client/tour.route.js
+++ routes/client/tour.route.js
@@ -4,4 +4,6 @@ const tourController = require("../../controllers/client/tour.controller");
 // router.get("/tours", tourController.list);
 router.get("/", tourController.list); // DO BÊN FILE index.route.js đã có đường dẫn /tours rồi nên bên này ko cần thêm vào nx
 
+router.get("/detail", tourController.detail);
+
 module.exports = router;
